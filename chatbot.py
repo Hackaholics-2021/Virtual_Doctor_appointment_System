eff148ab177efa351960c00ddb6699f8ed044076
Gaunisha-Gaanavi-G <86108368+Gaunisha-Gaanavi-G@users.noreply.github.com>
2021-11-15 19:55:05 +0530

    push

--- chatbot.py
+++ chatbot.py
@@ -8,6 +8,7 @@ def message_probability(user_message, recognised_words, single_response=False, r
 
     # Counts how many words are present in each predefined message
     for word in user_message:
+        
         if word in recognised_words:
             message_certainty += 1
 
@@ -36,11 +37,40 @@ def check_all_messages(message):
         highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
 
     # Responses -------------------------------------------------------------------------------------------------------
-    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
+    response('Hello, tell the symptoms you have!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
     response('See you!', ['bye', 'goodbye'], single_response=True)
     response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
     response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
     response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
+
+    # fever
+    response('How long have you had it?', ['i', 'have', 'fever'], required_words=['fever'])
+    response('So sorry to hear that. Tell me your age.', ['i', 'have', 'fever','for 3','days'], required_words=['days'])
+    response('Good. Let me know your body temperature.(in Fahrenheit)', ['years','months'], required_words=['years'])
+    response('Do you have any other of the following symptoms?<br>Cold, Sore throat, Headache, Running nose,None of these', ['c','f'], required_words=[degree])
+    response(long.R_FEVER+long.R_END, ['yes','cold','sore throat','headache','running nose','none of these'],required_words=[])
+
+    # cold
+    response('So sorry to hear that. Do you often get cold and do you think it due to any allergies?', ['i', 'have', 'cold'], required_words=['cold'])
+    response('If it is due to allergies then please take an allergy test and get to know what are you allergic for and try to stay away from it. For now. Please do the following'+long.R_COUGH+long.R_END, ['yes'], required_words=['yes'])
+    response(long.R_COUGH+long.R_END, ['no'],required_words=['no'])
+
+    # neck pain
+    response('On a scale of 1 to 10, how would you rate your pain?', ['i', 'have', 'neck', 'pain'], required_words=['pain'])
+    response(long.R_NECK_PAIN+long.R_END, ['1','2','3','4','5','6','7','8','9','10'], required_words=[degree])
+
+    # dandruff
+    response('Sorry to hear that.'+long.R_DANDRUFF+long.R_END, ['i', 'have', 'dandruff'], required_words=['dandruff'])
+
+    # constipation
+    response('Sorry to hear that? Tell me your diet plan. Are you a fast food eater? or Home food eater?', ['i', 'have', 'constipation'], required_words=['constipation'])
+    response(long.R_CONSTIPATION+long.R_END, ['Home','food','fast','eater'], required_words=['home','fast'])
+
+    # wounds and bruises
+    response('Sorry to hear that? Do you have blood leak or only skin bruises?', ['i', 'have', 'wound','bruises'], required_words=['wound'])
+    response(long.R_WOUNDS+long.R_END, ['blood', 'leak','bruises'], required_words=['leak'])
+    response(long.R_BRUISES+long.R_END, ['blood', 'leak','bruises'], required_words=['bruises'])
+
 
     # Longer responses
     response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
@@ -52,10 +82,17 @@ def check_all_messages(message):
 
     return long.unknown() if highest_prob_list[best_match] < 1 else best_match
 
-
+degree = 0.0
 # Used to get the response
 def get_response(user_input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    numbers = re.search(r"\d+", user_input.lower())
+    if numbers:
+        print(numbers.group())
+        numbers = numbers.group()
+        global degree
+        degree = numbers
+
     response = check_all_messages(split_message)
     return response
 
--- long_responses.py
+++ long_responses.py
@@ -2,12 +2,21 @@ import random
 
 R_EATING = "I don't like eating anything because I'm a bot obviously!"
 R_ADVICE = "If I were you, I would go to the internet and type exactly what you wrote there!"
-
+R_FEVER = "You can do the following:<br>1. Take Paracetamol for 3 days after meal in the morning and night.<br>2. Take Bismol for 1 day at night after meal."
+R_COUGH = 'You can do the following:<br>1. Take crocin for 3 days after meal in the morning and night.<br>2. Steam inhalation 3 times a day.'
+R_NECK_PAIN = 'You can do the following:<br>1. Apply Iodex or any other balm for pain releif.<br>2. Use ice pack on the area of pain.'
+R_DANDRUFF = 'I would recommend to use a good shampoo like Scalpe plus.Use it once for three days in 4 weeks.'
+R_CONSTIPATION = 'Don\'t worry I will help you. If you are a fast food eater then, please reduce the intake of fast foods and try converting into staple foods like oats, rice and wheat and drink plenty of water.Try to go out for a walk.<br> If you are a home food eater use Dulcoflex Stool Softener pills.'
+R_WOUNDS = 'Here\'s what you can do. Try washing the wounded area and apply disinfectant like detol. Clean and Dress the wound. If blood leak stops then it is alright!<br> But if blood leakage is high please visit a doctor immediately'
+R_BRUISES = 'Here\'s what you can do. Try washing the wounded area and apply disinfectant like detol. Small bruises will heal as it dries. If bleeding occurs please address a dcotor.'
+R_END = '<br><br> <i>Please note:<br>This isn\'t a diagnosis. Always visit a doctor if you\'re unsure, or if symptoms get worse, or don\'t improve. If your situation is serious, call an emergency service.</i> <br><br><br><b>If you think you want consultation please visit consult tab for consulting with top doctors</b><br><br> Or if you want to book an appointment with our top doctors, feel free to go into <b>Book Appointments</b> tab for booking appointment'
 
 def unknown():
     response = ["Could you please re-phrase that? ",
-                "...",
-                "Sounds about right.",
-                "What does that mean?"][
-        random.randrange(4)]
+                "Sorry, I couldn't get you :(",
+                "Let's try one more time. Can you please tell once again?",
+                "What does that mean?",
+                "I'm not quite sure what do you mean. Sorry:(",
+                "I'm just a Bot. Try something new."][
+        random.randrange(6)]
     return response
